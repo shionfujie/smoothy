d1f2ab1bcc137700b872a3efee4822d8463009a7
shion.t.fujie <tonaluna32966@gmail.com>
2020-12-28 15:15:47 +0900

    stream of consecutive siblings

--- src/main/scala/io/s19f/smoothy/jsoup/nodes/nodes.scala
+++ src/main/scala/io/s19f/smoothy/jsoup/nodes/nodes.scala
@@ -7,11 +7,16 @@ package object nodes {
   import org.jsoup.nodes.Element
 
   class ElementOps(el: Element) {
-      
+
     def $(selector: String) = el.selectFirst(selector)
 
     def $$(selector: String) = el.select(selector)
-    
+
+    def nextElementSiblings: Stream[Element] =
+      Stream
+        .iterate(el.nextElementSibling)(_.nextElementSibling)
+        .takeWhile(el => el != null)
+
   }
 
   implicit def elementOps(el: Element) = new ElementOps(el)
