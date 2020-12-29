e33d6660bb80032d76452284a0ba5029f96d0004
shion.t.fujie <tonaluna32966@gmail.com>
2020-12-29 09:47:30 +0900

    allow formatter to inject custom style

--- src/main/scala/io/s19f/smoothy/format/els.scala
+++ src/main/scala/io/s19f/smoothy/format/els.scala
@@ -8,24 +8,31 @@ import org.jsoup.nodes.Element
 object els {
 
   def listItem(el: Element) =
-    el2text _ andThen md.listItem _
+    defaultListItemFormatter
 
-  def listItems(els: Traversable[Element]) =
-    listItemsDepths(els.map((0, _)))
+  def listItems(els: Traversable[Element], formatter: PartialFunction[Element, String] = Map.empty): String =
+    listItemsDepths(els.map((0, _)), formatter)
 
-  def listItemsDepths(els: Traversable[(Int, Element)]) =
-    md.listItemsDepths(for { (depth, el) <- els } yield (depth, el2text(el)))
+  def listItemsDepths(els: Traversable[(Int, Element)], formatter: PartialFunction[Element, String] = Map.empty) =
+    indent(for { (depth, el) <- els } yield (depth, (formatter orElse defaultListItemFormatter)(el)))
 
-  def firstSentences(els: Traversable[Element])         =
+  def firstSentences(els: Traversable[Element]) =
     els.map(_.text.trim.sentences(0)).mkString("\n")
 
-  private def el2text(el: Element) = {
-    val title = el.text.trim.title
-    el.normalName match {
-      case "a" => md.link(title, el.absUrl("href"))
-      case "p" => el.text.trim.sentences(0)
-      case _   => title
-    }
+  private val defaultFormatter: PartialFunction[Element, String] = {
+    case el =>
+      val title = el.text.trim.title
+      el.normalName match {
+        case "a" => md.link(title, el.absUrl("href"))
+        case "p" => el.text.trim.sentences(0)
+        case _   => title
+      }
   }
+
+  private val defaultListItemFormatter =
+    defaultFormatter andThen md.listItem
+
+  private def indent(els: Traversable[(Int, String)]) =
+    (for ((depth, text) <- els) yield "  " * depth + text).mkString("\n")
 
 }
